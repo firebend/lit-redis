c5ca751399c3e8068a44add6d241fc23e660d34d
Jacob Atchley <yaaqob.atchley@gmail.com>
2021-06-07 11:04:35 -0500

    wip init

---
+++ LitRedis.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LitRedis.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
